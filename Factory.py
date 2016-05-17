b367dfb83e788fb041639fc9f7815f551bdfde8c
Marcin <JediMarcinC@users.noreply.github.com>
2016-05-17 18:28:29 +0200

    Add files via upload
    New file in this repository

---
+++ Factory.py
@@ -0,0 +1,30 @@
+class Ice:
+    def getName(self):
+        return self.name
+
+    def __str__(self):
+        return self.name+", "+self.taste+", "+self.wafer
+
+
+class Choco(Ice):
+    def __init__(self):
+        self.name = "SuperChoco"
+        self.taste = "chocolate"
+        self.wafer ="danish"
+
+
+class Vanilla(Ice):
+    def __init__(self):
+        self.name = "Standard"
+        self.taste = "vanilla"
+        self.wafer ="classic"
+
+class Factory:
+    def makeIce(self, kind):
+        if kind=='choco':
+            return Choco()
+        elif kind=='vanil':
+            return Vanilla()
+
+f=Factory()
+print(f.makeIce('choco'))
